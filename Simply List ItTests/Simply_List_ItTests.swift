9860c5cf0d7b0c5d95f699cb8192cf315005411f
Mark Stuver <mark@stuverfamily.com>
2015-08-08 13:27:23 -0400

    Initial Commit

---
+++ Simply List ItTests/Simply_List_ItTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simply_List_ItTests.swift
+//  Simply List ItTests
+//
+//  Created by Mark Stuver on 8/8/15.
+//  Copyright (c) 2015 Mark Stuver. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Simply_List_ItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
